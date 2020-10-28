0beef6f99e12dc898001b4ecc91eb25ccd04f349
Crowdbotics <team@crowdbotics.com>
2020-10-28 14:50:04 +0000

    Initial commit.

---
+++ ios/riptide_biofuels_22056Tests/riptide_biofuels_22056Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface riptide_biofuels_22056 : XCTestCase
+
+@end
+
+@implementation riptide_biofuels_22056
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
